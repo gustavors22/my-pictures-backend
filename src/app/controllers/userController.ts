11d121283059ee61defdc5945b0023add90fbb92
Gustavo Reis <gustavoreissilva.2016@gmail.com>
2022-01-26 22:57:11 -0300

    inserindo user na db

--- src/app/controllers/userController.ts
+++ src/app/controllers/userController.ts
@@ -1,14 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
+const process = require("process");
 
 class UserController {
     public async store(request: Request, response: Response) {
-        const { name, email, password } = request.body;
+        let { name, email, password } = request.body;
+
+        const saltRounds = process.env.SALT_ROUNDS;
         
-        const newPassword = await bcrypt.hash(password, env("SALT_ROUNDS"));
-        
+        password = bcrypt.hashSync(password, parseInt(saltRounds));
+
         const user = await prisma.user.create({
             data: {
                 name,
